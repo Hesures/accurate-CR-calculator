e6c3f4cbef86b599b58fc60265f63971b409a910
Hesures <128929471+Hesures@users.noreply.github.com>
2023-05-09 12:15:24 +0200

    Accurate CR calculator v.1.0.1
    Const variables added

---
+++ index.js
@@ -0,0 +1,7 @@
+const numberOfCharacters = document.querySelector("#numberOfCharacters");
+const levelOfCharacters = document.querySelector("#levelOfCharacters");
+const numberOfMonsters = document.querySelector("#numberOfMonsters");
+const xpOfMonsters = document.querySelector("#xpOfMonsters");
+
+
+console.log(numberOfCharacters.value);
