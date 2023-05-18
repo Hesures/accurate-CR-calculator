f8908a3c581010fabed75f747ec638cbca80bffb
Hesures <128929471+Hesures@users.noreply.github.com>
2023-05-18 08:08:55 +0200

    "for" added

--- index.js
+++ index.js
@@ -5,33 +5,55 @@ const xpOfMonsters = document.querySelector("#xpOfMonsters");
 
 const calculate = document.querySelector("#calculate");
 
-// calculate.addEventListener("click", function (){
-//     console.log(numberOfCharacters.value * levelOfCharacters.value);    
-//     console.log(numberOfMonsters.value * xpOfMonsters.value);
-// });
-
-var chartOfDificulty = [/*1*/[25,50,75,100],
-                        /*2*/[50,100,150,200],
-                        /*3*/[75,150,225,400],
-                        /*4*/[125,250,375,500],
-                        /*5*/[250,500,750,1100],
-                        /*6*/[300,600,900,1400],
-                        /*7*/[350,750,1100,1700],
-                        /*8*/[450,900,1400,2100],
-                        /*9*/[550,1100,1600,2400],
-                        /*10*/[600,1200,1900,2800],
-                        /*11*/[800,1600,2400,3600],
-                        /*12*/[1000,2000,3000,4500],
-                        /*13*/[1100,2200,3400,5100],
-                        /*14*/[1250,2500,3800,5700],
-                        /*15*/[1400,2800,4300,6400],
-                        /*16*/[1600,3200,4800,7200],
-                        /*17*/[2000,3900,5900,8800],
-                        /*18*/[2100,4200,6300,9500],
-                        /*19*/[2400,4900,7300,10900],
-                        /*20*/[2800,5700,8500,12700]];
+var chartOfDifficulty = 
+[/*lvl 1*/[25,50,75,100],
+/*lvl 2*/[50,100,150,200],
+/*lvl 3*/[75,150,225,400],
+/*lvl 4*/[125,250,375,500],
+/*lvl 5*/[250,500,750,1100],
+/*lvl 6*/[300,600,900,1400],
+/*lvl 7*/[350,750,1100,1700],
+/*lvl 8*/[450,900,1400,2100],
+/*lvl 9*/[550,1100,1600,2400],
+/*lvl 10*/[600,1200,1900,2800],
+/*lvl 11*/[800,1600,2400,3600],
+/*lvl 12*/[1000,2000,3000,4500],
+/*lvl 13*/[1100,2200,3400,5100],
+/*lvl 14*/[1250,2500,3800,5700],
+/*lvl 15*/[1400,2800,4300,6400],
+/*lvl 16*/[1600,3200,4800,7200],
+/*lvl 17*/[2000,3900,5900,8800],
+/*lvl 18*/[2100,4200,6300,9500],
+/*lvl 19*/[2400,4900,7300,10900],
+/*lvl 20*/[2800,5700,8500,12700]];
 
 
-// calculate.addEventListener("click", function (){
-//     console.log(chartOfDificulty[levelOfCharacters.value-1]);
-// });
+calculate.addEventListener("click", function (){
+    
+    // number of difficulty (Easy, Medium, Hard, Deadly) per level
+    var difficultyPerLevel = chartOfDifficulty[levelOfCharacters.value-1]; 
+
+    // quantity/value of the number of characters
+    var quantity = numberOfCharacters.value;
+
+    //REPETIDAS ESTAS LÍNEAS MÁS ABAJO
+    for (var i = 0; i <= 3; i++) {
+        console.log(quantity * difficultyPerLevel[i]);}   
+
+    // ¿Introducir un foreach de difficultyPerLevel?
+    // ¿También se podría construir usando los datos introducidos como parámetros de una función? Ej: function (x, y), donde x e y son nivel, array, xp, etc... 
+    
+
+//CORTAR ESTO ANTES DE SUBIR A GITHUB Y DESPUÉS PEGAR Y EXPERIMENTAR
+        // for (var i = 0; i <= 3; i++) {
+        // var challenge = numberOfMonsters.value * xpOfMonsters.value;
+        // console.log(quantity * difficultyPerLevel[i]);   
+        // if (challenge <= quantity * difficultyPerLevel[i]) {
+        //     console.log("Easy-Medium");
+        // } else {
+        //         console.log("Difficult");
+        //     }
+        // }
+    }
+
+);
